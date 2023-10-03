21ae507f33bf516ceab3ef7cdb4cdb0b4af9a985
intmax <563748846@qq.com>
2023-10-03 17:14:23 -0600

    modify

--- aplha_beta.py
+++ aplha_beta.py
@@ -1,6 +1,8 @@
 from board import GoBoard, GO_POINT
 from board_base import GO_COLOR, opponent, EMPTY, BORDER, BLACK
 import numpy as np
+from board_util import GoBoardUtil
+
 
 class LookUpTable:
     def __init__(self) -> None:
@@ -9,55 +11,78 @@ class LookUpTable:
     def __repr__(self):
         return self.table.__repr__()
 
-    def store(self, board:np.ndarray, value, goboard:GoBoard):
+    def store(self, board: np.ndarray, value, goboard: GoBoard):
         self.table[(tuple(board))] = (value, goboard)
 
-    def look_up(self, board:np.ndarray):
+    def look_up(self, board: np.ndarray):
         return self.table.get(tuple(board))
 
-
-def next_two(list_current:list, board:GoBoard, current_player:GO_COLOR, move:GO_POINT):
-        d1 = 0
-        d2 = 0
-        index = list_current.index(move)
-        if 0 <= (index + 3) < len(list_current):
-            if board.get_color(list_current[index + 1]) == opponent(current_player) \
-                and board.get_color(list_current[index + 2]) == EMPTY \
-                and board.get_color(list_current[index + 3]) == EMPTY:
-                d1 = 0.25
-            if board.get_color(list_current[index + 2]) == opponent(current_player)\
-                and board.get_color(list_current[index + 1]) == opponent(current_player) \
-                and board.get_color(list_current[index + 3]) == EMPTY:
-                d1 = 0.5
-        if 0 <= (index - 3) < len(list_current):
-            if board.get_color(list_current[index - 1]) == opponent(current_player) \
-                and board.get_color(list_current[index - 2]) == EMPTY \
-                and board.get_color(list_current[index - 3]) == EMPTY:
-                d2 = 0.25
-            if list_current[index - 2] == opponent(current_player) \
-                and board.get_color(list_current[index - 1]) == opponent(current_player) \
-                and board.get_color(list_current[index - 3]) == EMPTY:
-                d2 = 0.5 
-        return max(d1, d2)
-
-def connect_cal(
-        list_current: list, board: GoBoard, current_player: GO_COLOR
-    ) -> int:
-        U = -1
-        prev = BORDER
-        counter = 1
-        for stone in list_current:
-            if board.get_color(stone) == prev and prev == current_player:
-                counter += 1
-            else:
-                if counter > U:
-                    U = counter
-                counter = 1
-                prev = board.get_color(stone)
-        if counter != -1:
-            return counter
+def is_unbeatable_sequence(list_current: list, board: GoBoard, player: GO_COLOR) -> bool:
+    seq_length = 0
+    for idx, point in enumerate(list_current):
+        if board.get_color(point) == player:
+            seq_length += 1
+            if seq_length == 4:
+                # Check if there's a space before and after the sequence
+                if idx - 4 < 0 or board.get_color(list_current[idx - 4]) == EMPTY:
+                    if idx + 1 >= len(list_current) or board.get_color(list_current[idx + 1]) == EMPTY:
+                        return True
         else:
-            return 0
+            seq_length = 0
+    return False
+
+def next_two(
+    list_current: list, board: GoBoard, current_player: GO_COLOR, move: GO_POINT
+):
+    d1 = 0
+    d2 = 0
+    index = list_current.index(move)
+    if 0 <= (index + 3) < len(list_current):
+        if (
+            board.get_color(list_current[index + 1]) == opponent(current_player)
+            and board.get_color(list_current[index + 2]) == EMPTY
+            and board.get_color(list_current[index + 3]) == EMPTY
+        ):
+            d1 = 0.25
+        if (
+            board.get_color(list_current[index + 2]) == opponent(current_player)
+            and board.get_color(list_current[index + 1]) == opponent(current_player)
+            and board.get_color(list_current[index + 3]) == EMPTY
+        ):
+            d1 = 0.5
+    if 0 <= (index - 3) < len(list_current):
+        if (
+            board.get_color(list_current[index - 1]) == opponent(current_player)
+            and board.get_color(list_current[index - 2]) == EMPTY
+            and board.get_color(list_current[index - 3]) == EMPTY
+        ):
+            d2 = 0.25
+        if (
+            list_current[index - 2] == opponent(current_player)
+            and board.get_color(list_current[index - 1]) == opponent(current_player)
+            and board.get_color(list_current[index - 3]) == EMPTY
+        ):
+            d2 = 0.5
+    return max(d1, d2)
+
+
+def connect_cal(list_current: list, board: GoBoard, current_player: GO_COLOR) -> int:
+    U = -1
+    prev = BORDER
+    counter = 1
+    for stone in list_current:
+        if board.get_color(stone) == prev and prev == current_player:
+            counter += 1
+        else:
+            if counter > U:
+                U = counter
+            counter = 1
+            prev = board.get_color(stone)
+    if counter != -1:
+        return counter
+    else:
+        return 0
+
 
 def h_fun(board: GoBoard, move: GO_POINT, current_player: GO_COLOR) -> int:
     h_value = 5
@@ -67,35 +92,49 @@ def h_fun(board: GoBoard, move: GO_POINT, current_player: GO_COLOR) -> int:
     board_copy_op = board.copy()
     board_copy.play_move(move, opponent(current_player))
     h_value_cap = min(h_value, ((10 - board_copy.get_captures(current_player)) / 2))
-    h_cap_op = min(h_value, ((10 - board_copy_op.get_captures(opponent(current_player))) / 2))
+    h_cap_op = min(
+        h_value, ((10 - board_copy_op.get_captures(opponent(current_player))) / 2)
+    )
 
     for row in board.rows:
         if move in row:
             counter = connect_cal(row, board_copy, current_player)
             counter_op = connect_cal(row, board_copy_op, opponent(current_player))
             h_tmp = h_value_cap - next_two(row, board_copy, current_player, move)
-            h_tmp_op = h_cap_op - next_two(row, board_copy_op, opponent(current_player), move)
+            h_tmp_op = h_cap_op - next_two(
+                row, board_copy_op, opponent(current_player), move
+            )
             h_value = min(h_value, (5 - counter), (5 - counter_op), h_tmp, h_tmp_op)
     for col in board.cols:
         if move in col:
             counter = connect_cal(col, board_copy, current_player)
             counter_op = connect_cal(col, board_copy_op, opponent(current_player))
             h_tmp = h_value_cap - next_two(col, board_copy, current_player, move)
-            h_tmp_op = h_cap_op - next_two(col, board_copy_op, opponent(current_player), move)
+            h_tmp_op = h_cap_op - next_two(
+                col, board_copy_op, opponent(current_player), move
+            )
             h_value = min(h_value, (5 - counter), (5 - counter_op), h_tmp, h_tmp_op)
     for diag in board.diags:
         if move in diag:
             counter = connect_cal(diag, board_copy, current_player)
             counter_op = connect_cal(diag, board_copy_op, opponent(current_player))
             h_tmp = h_value_cap - next_two(diag, board_copy, current_player, move)
-            h_tmp_op = h_cap_op - next_two(diag, board_copy_op, opponent(current_player), move)
+            h_tmp_op = h_cap_op - next_two(
+                diag, board_copy_op, opponent(current_player), move
+            )
             h_value = min(h_value, (5 - counter), (5 - counter_op), h_tmp, h_tmp_op)
     return h_value
 
 
-def alpha_beta(board: GoBoard, alpha, beta, depth, color: GO_COLOR, table:LookUpTable):
+def alpha_beta(board: GoBoard, alpha, beta, depth, color: GO_COLOR, table: LookUpTable):
     if board.is_terminal() or depth == 0:
         return board.statically_evaluate(color), None, board
+    
+    for sequence in [board.rows, board.cols, board.diags]:
+        for line in sequence:
+            if is_unbeatable_sequence(line, board, opponent(color)):
+                winner = opponent(color)
+                return -1, None, board
 
     best_move = None
     best_board = None
@@ -111,7 +150,9 @@ def alpha_beta(board: GoBoard, alpha, beta, depth, color: GO_COLOR, table:LookUp
         if look_up is not None and look_up[1] >= depth:
             value = look_up[0]
         else:
-            res = alpha_beta(board_copy, -beta, -alpha, depth - 1, opponent(color), table)
+            res = alpha_beta(
+                board_copy, -beta, -alpha, depth - 1, opponent(color), table
+            )
             value = -res[0]
             table.store(board_copy.board, value, depth)
 
